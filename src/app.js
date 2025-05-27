6ccafbb4c866cba24e5fdedc1c6f02dbd8966678
Yayiyi94 <yadiratr94@gmail.com>
2025-05-26 18:12:32 -0600

    Add API main functions

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let tempeElement = document.querySelector("#weather-app-temperature");
+  tempeElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "10fa90a2o832483bf734tfe8a27fcdad";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Mexico City");
